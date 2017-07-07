83a2666d24284b650c63ae6ad2807d494d9a4405
sanjayng007 <sanjayng007@gmail.com>
2017-07-07 13:09:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+app.set("view engine" , "ejs");
+app.use("/assets", express.static("assets"));
+
+app.get("/",function(req,res){
+	res.render("index");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on');
+});
+// app.listen(8080);
+// console.log("i ran");
